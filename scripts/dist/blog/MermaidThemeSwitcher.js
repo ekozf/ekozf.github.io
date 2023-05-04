be2a3b46a3ebe915eebb59e4d38dc6ede1ccc5dc
ekozf <50245213+ekozf@users.noreply.github.com>
2023-05-05 00:10:29 +0200

    Completely restructure the website, allow for main portfolio page, blog becomes subdir

---
+++ scripts/dist/blog/MermaidThemeSwitcher.js
@@ -0,0 +1,25 @@
+import mermaid from "/resources/libs/mermaid/mermaid.esm.min.mjs";
+const mermaidElement = document.querySelector(".mermaid");
+if (mermaidElement) {
+    const txt = document.createElement("textarea");
+    txt.innerHTML = mermaidElement.innerHTML;
+    const svgValue = txt.value;
+    window.mermaidGeneratedSvg = svgValue;
+    window.renderMermaid = async () => {
+        document.querySelector("#mermaid-output")?.remove();
+        const mermaidConfig = { startOnLoad: false, theme: "dark" };
+        if (localStorage.getItem("dark-mode") === "false") {
+            mermaidConfig.theme = "default";
+        }
+        mermaid.initialize(mermaidConfig);
+        var output = document.createElement("div");
+        output.id = "mermaid-output";
+        mermaidElement.parentNode.appendChild(output);
+        const insertSvg = (svgCode) => {
+            mermaidElement.innerHTML = svgCode;
+        };
+        await mermaid.mermaidAPI.renderAsync("mermaid-output", window.mermaidGeneratedSvg, insertSvg);
+    };
+    window.renderMermaid();
+}
+//# sourceMappingURL=MermaidThemeSwitcher.js.map
